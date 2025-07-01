5992813ccae2252c2af7b76c154bdebf5b60a784
anavoro <voropayeva.a@gmail.com>
2025-07-01 11:12:14 +0300

    Negative tests for editing user profile

--- utils/fixtures.ts
+++ utils/fixtures.ts
@@ -1,13 +1,20 @@
 import { test as base } from '@playwright/test';
+import { HomePage } from '../pages/homePage';
 import { AdminPage } from '../pages/adminPage';
 import { AdminCategoryPage } from '../pages/adminCategoryPage';
 import { AdminManufacturerPage } from '../pages/adminManufacturerPage';
 import 'dotenv/config';
+import { LoginPage } from '../pages/loginPage';
+import { ProfilePage } from '../pages/profilePage';
+import { texts } from '../constants/authData';
 
 type MyFixtures = {
   loggedInAdmin: AdminPage;
   adminCategoryPage: AdminCategoryPage;
   adminManufacturerPage: AdminManufacturerPage;
+  loggedInUser: HomePage;
+  profilePage: ProfilePage;
+  nameFields: Array<{ label: string; expectedError: string }>;
 };
 
 export const test = base.extend<MyFixtures>({
@@ -16,7 +23,6 @@ export const test = base.extend<MyFixtures>({
     const adminEmail = process.env.ADMIN_EMAIL!;
     const adminPassword = process.env.ADMIN_PASSWORD!;
     const adminUrl = process.env.ADMIN_URL!;
-    
     await page.goto(adminUrl);
     await adminPage.login(adminEmail, adminPassword);
     await use(adminPage);
@@ -26,9 +32,26 @@ export const test = base.extend<MyFixtures>({
     const adminCategoryPage = new AdminCategoryPage(page);
     await use(adminCategoryPage);
   },
-
+  
   adminManufacturerPage: async ({ page, loggedInAdmin}, use) => {
     const adminManufacturerPage = new AdminManufacturerPage(page);
     await use(adminManufacturerPage);
-  }
+  },
+  
+  loggedInUser: async ({ page }, use) => {
+    const homePage = new HomePage(page);
+    const loginPage = new LoginPage(page);
+    const testEmail = process.env.TEST_EMAIL!;
+    const testPassword = process.env.TEST_PASSWORD!;
+    await page.setViewportSize({ width: 1536, height: 864 });
+    await homePage.goto("/");
+    await homePage.clickLogin();
+    await loginPage.login(testEmail, testPassword);
+    await use(homePage);
+  },
+
+  profilePage: async ({ page }, use) => {
+    const profilePage = new ProfilePage(page);
+    await use(profilePage);
+  },
 });
